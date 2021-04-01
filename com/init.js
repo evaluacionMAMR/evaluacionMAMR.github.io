7a7c19b4291a9d343a496e84ba5988ca776c8b73
evaluacionMAMR <81721175+evaluacionMAMR@users.noreply.github.com>
2021-03-31 18:25:16 -0600

    Create init.js

---
+++ com/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+   apiKey: "AIzaSyD9DnYBev6CRB1jtSH-5nwZcwyrSeCuFrA",
+    authDomain: "examen41.firebaseapp.com",
+    projectId: "examen41",
+    storageBucket: "examen41.appspot.com",
+    messagingSenderId: "918881310870",
+    appId: "1:918881310870:web:4396431f4de9233c1fcab7",
+    measurementId: "G-CFBC33HNQD"
+});
