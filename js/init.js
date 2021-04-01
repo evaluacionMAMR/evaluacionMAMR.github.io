331754add830816bf9b47c1397b38ec1000f147a
evaluacionMAMR <81721175+evaluacionMAMR@users.noreply.github.com>
2021-03-31 18:21:28 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD9DnYBev6CRB1jtSH-5nwZcwyrSeCuFrA",
+   apiKey: "AIzaSyD9DnYBev6CRB1jtSH-5nwZcwyrSeCuFrA",
     authDomain: "examen41.firebaseapp.com",
     projectId: "examen41",
     storageBucket: "examen41.appspot.com",
     messagingSenderId: "918881310870",
-    appId: "1:918881310870:web:b796decc21f3e1791fcab7",
-    measurementId: "G-N1TQTVFFF6"
+    appId: "1:918881310870:web:4396431f4de9233c1fcab7",
+    measurementId: "G-CFBC33HNQD"
 });
